b1508b472a207cab41a54c636579ddf05503d1c0
fehomeh <fehomehal@gmail.com>
2013-06-18 22:54:26 +0300

    Added policy foo template and controller action for it

--- path/src/Insurance/ContentBundle/Controller/PDFController.php
+++ path/src/Insurance/ContentBundle/Controller/PDFController.php
@@ -3,6 +3,7 @@ namespace Insurance\ContentBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Insurance\ContentBundle\Entity\Feedback;
+use Symfony\Component\HttpFoundation\Response;
 
 class PDFController extends Controller {
 
@@ -13,6 +14,16 @@ class PDFController extends Controller {
         } catch (Excepltion $e){
             return $this->render($e->getMessage(), 404);
         }
+    }
+
+    public function notifyUserPolicy($orderId)
+    {
+      try {
+        $order = $this->getDoctrine()->getRepository('InsuranceContentBundle:InsuranceOrder')->findOne($orderId);
+        return $this->render('InsuranceContentBundle:PDFController:userPolicy.html.twig', array('order' => $order));
+      } catch (Exception $e){
+        return $this->render(new Response ($e->getMessage(), 404));
+      }
     }
 }
 
